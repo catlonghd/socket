145c1a198f4e030c120807a9063bd49a6b4cc8a1
dangth2002@gmail.com <dangth2002@gmail.com>
2021-12-04 10:22:08 +0700

    log in / sign up

---
+++ client1.py
@@ -0,0 +1,31 @@
+import socket
+
+
+
+# create an ipv4 (AF_INET) socket object using the tcp protocol (SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+# client.connect((target, port))
+HOST = input("Input IP server: ")
+PORT = 1233
+client.connect((HOST, PORT))
+response = client.recv(2048)
+# Input UserName
+name = input(response.decode())	
+client.send(str.encode(name))
+response = client.recv(2048)
+# Input Password
+password = input(response.decode())	
+client.send(str.encode(password))
+''' Response : Status of Connection :
+	1 : Registeration successful 
+	2 : Connection Successful
+	3 : Login Failed
+'''
+# Receive response 
+response = client.recv(2048)
+response = response.decode()
+
+print(response)
+client.close()
